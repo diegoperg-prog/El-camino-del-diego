c39008d99d838382dc6a9a9f21f5f3d9e7a441c4
diegoperg-prog <diego.perg@gmail.com>
2025-10-25 02:03:59 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app-background">
+      <h1>El Camino del Diego+</h1>
+      <p>Tu viaje del héroe comienza aquí.</p>
+    </div>
+  );
+}
